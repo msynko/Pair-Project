1c72b3703fad0114d5b99b7437735ac7138327c6
msynko <marina.synko@gmail.com>
2018-12-11 14:20:15 -0500

    Add new form

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,9 @@
 class Product < ApplicationRecord
+
 validates :name, presence: true
-validates :price_in_cents, numericality: { only_integer: true }
+validates :description, presence: true
+validates :price_in_cents, presence: true
+
+validates :price_in_cents, numericality:{ only_integer: true}
+
 end
