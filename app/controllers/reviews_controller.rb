72f79a2d62619e2a0997336e6a996324ba2eb4b0
victoriak94 <vkamila6@gmail.com>
2018-12-11 17:59:01 -0500

    Rendered edit page for review. Create delete links for reviews

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -3,34 +3,41 @@ class ReviewsController < ApplicationController
   def create
     @review = Review.new
     @review.comment = params[:review][:comment]
+    @product = Product.find(params[:product_id])
+    @review.product = @product
 
     if @review.save
-      redirect_to product_path, notice: "You have created a new review!"
+      redirect_to product_path(@product), notice: "You have created a new review!"
     else
-      render :new
+      render product_path(@product)
     end
   end
 
   def edit
     @review = Review.find(params[:id])
+    @product = Product.find(params[:product_id])
+    @review.product = @product
   end
 
   def update
     @review = Review.find(params[:id])
-
+    @product = Product.find(params[:product_id])
+    @review.product = @product
     @review.comment = params[:review][:comment]
 
     if @review.save
       redirect_to product_path(@product), notice: "Review has been updated."
     else
-      render :edit
+      render edit_product_review_path
     end
   end
 
   def destroy
     @review = Review.find(params[:id])
+    @product = Product.find(params[:product_id])
+    @review.product = @product
     @review.destroy
-    redirect_to product_path, notice: "Review has been deleted."
+    redirect_to product_path(@product), notice: "Review has been deleted."
   end
 
 end
