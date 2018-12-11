f81b40c1a2b2351548946bc58342c8063bc11e06
victoriak94 <vkamila6@gmail.com>
2018-12-11 14:56:07 -0500

    Create edit product form and edit link on index page. Create destroy method and create delete link on index page

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,5 @@
 class ProductsController < ApplicationController
 
-
   def index
     @products=Product.all
   end
@@ -28,16 +27,27 @@ class ProductsController < ApplicationController
   end
 
   def edit
-
+    @product = Product.find(params[:id])
   end
 
   def update
+    @product = Product.find(params[:id])
 
+    @product.name = params[:product][:name]
+    @product.description = params[:product][:description]
+    @product.price_in_cents = params[:product][:price_in_cents]
+
+    if @product.save
+      redirect_to "/products/#{@product.id}"
+    else
+      render :edit
+    end
   end
 
-  def delete
-
+  def destroy
+    @product = Product.find(params[:id])
+    @product.destroy
+    redirect_to "/products"
   end
-
 
 end
